a47af77a1eb7d6557662128bdbe5f707a06fb1b8
bokyung <bbookng@gmail.com>
2023-09-16 01:35:10 +0900

    [PGS] Lv2 / 기능개발

--- Java/Programmers/Solution45.java
+++ Java/Programmers/Solution45.java
@@ -1,2 +1,31 @@
-package Programmers;public class Solution45 {
+package Programmers;
+
+import java.util.*;
+
+class Solution45 {
+    public int[] solution(int[] progresses, int[] speeds) {
+
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+
+        int day = 1;
+        int cnt = 0;
+        int index = 0;
+
+        while (index < progresses.length) {
+            if (progresses[index] + (speeds[index] * day) >= 100) {
+                index ++;
+                cnt ++;
+            } else {
+                day += 1;
+                if (cnt > 0) {
+                    answer.add(cnt);
+                    cnt = 0;
+                }
+            }
+        }
+
+        answer.add(cnt);
+
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
 }
