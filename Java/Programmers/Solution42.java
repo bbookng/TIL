9c0bd1726ef62457d0eef295d763340839e08e18
bokyung <bbookng@gmail.com>
2023-09-18 09:29:42 +0900

    [PGS] Lv2 / 올바른 괄호

--- Java/Programmers/Solution42.java
+++ Java/Programmers/Solution42.java
@@ -1,2 +1,24 @@
-package Programmers;public class Solution42 {
+package Programmers;
+
+import java.util.*;
+
+class Solution42 {
+    boolean solution(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
 }
