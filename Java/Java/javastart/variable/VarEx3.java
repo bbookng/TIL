c08bb71c41ebb23ef4cce735d69583d368896273
bbookng <bbookng@gmail.com>
2024-08-18 22:29:02 +0900

    [Java] 김영한 자바 입문 - 변수

---
+++ Java/Java/javastart/variable/VarEx3.java
@@ -0,0 +1,11 @@
+package Java.javastart.variable;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long num = 10000000000L;
+        System.out.println(num);
+
+        boolean bool = true;
+        System.out.println(bool);
+    }
+}
